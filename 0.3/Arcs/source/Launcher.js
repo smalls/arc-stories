c384a22b645e9a6297f32f11b02d51e6217b0836
Scott J Miles <sjmiles@google.com>
2017-11-08 18:02:20 -0800

    fix icon size

--- 0.3/Arcs/source/Launcher.js
+++ 0.3/Arcs/source/Launcher.js
@@ -18,7 +18,7 @@ defineParticle(({DomParticle}) => {
 <style>
   [${host}] .material-icons {
     font-family: 'Material Icons';
-    font-size: 24px;
+    /*font-size: 24px;*/
     font-style: normal;
     -webkit-font-feature-settings: 'liga';
     -webkit-font-smoothing: antialiased;
